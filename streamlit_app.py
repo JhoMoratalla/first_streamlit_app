e4edd092676d5890b90ed2115300d5c4922dad11
Joanna Moratalla <109075203+JhoMoratalla@users.noreply.github.com>
2022-07-11 19:08:22 +0800

    Update streamlit_app.py
    Add MENU

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('BREAKFAST MENU')
+
+streamlit.text('Omega 3 & Blueberry Oatmel')
+streamlit.text('Kale, Spinach & Rocket SMoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
