5d7a144f26f6b5eff87e80f6d48fc4c4f06a85b6
Joanna Moratalla <109075203+JhoMoratalla@users.noreply.github.com>
2022-07-11 19:14:24 +0800

    Update streamlit_app.py
    Add ICON to the items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('BREAKFAST MENU')
 
-streamlit.text('Omega 3 & Blueberry Oatmel')
-streamlit.text('Kale, Spinach & Rocket SMoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmel')
+streamlit.text('🥗 Kale, Spinach & Rocket SMoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
